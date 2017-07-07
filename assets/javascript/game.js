4ae133af783fde9bc646f2640ba855df935f4e1f
Michael Osgood <michaelosgood@Michaels-MacBook-Pro.local>
2017-07-06 20:41:39 -0500

    still need to add logic

---
+++ assets/javascript/game.js
@@ -0,0 +1,35 @@
+// Michael Osgood / Gold Coins Collector Game / HW4 //
+
+//Global Variables//
+//-----------------------------------------------------------------------
+
+
+//Game Counters//
+//-----------------------------------------------------------------------
+
+
+//Functions//
+//-----------------------------------------------------------------------
+
+
+//Main Process//
+//-----------------------------------------------------------------------
+
+//PSUEDO CODE
+//Generate a random number between 30 and 70 for player to guess
+
+//Assign a random value between 1 through 12 for each coin
+
+//When player clicks on a coin, display the value in HTML
+
+//When another coin is clicked on, add the sum of all the coins
+	//If sum exceeds random number, player loses
+	//Add 1 to number of losses
+	//Reset random number
+
+	//If value is equal to random number
+	//Add 1 to number of wins
+	//Reset random number
+
+
+
